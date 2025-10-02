85032708eb76edd1af623598977cb83ac1e7234a
S4mur419020 <muraitomi04@gmail.com>
2025-10-02 15:34:41 +0200

    vezerlo, main

--- LogikaiLada/src/main/LogikaiLada.java
+++ LogikaiLada/src/main/LogikaiLada.java
@@ -1,11 +1,23 @@
 package main;
 
+//import modell.LadaModell;
+
 
 public class LogikaiLada {
-
+//    public LogikaiLada(){
+//        LadaModell modell=new LadaModell();
+//        
+//        java.awt.EventQueue.invokeLater(new Runnable(){
+//            public void run(){
+//                GuiNezet nezet=new GuiNezet();
+//                LogikaiLada vezerlo=new LogikaiLada(modell, nezet);
+//                
+//            }
+//        });
+//    }
     
     public static void main(String[] args) {
-        
+//        new LogikaiLada();
     }
     
 }
--- LogikaiLada/src/vezerlo/LogikaiLadaController.java
+++ LogikaiLada/src/vezerlo/LogikaiLadaController.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vezerlo;
 
-/**
- *
- * @author murai.tamas
- */
+//import modell.LadaModell;
+
+
 public class LogikaiLadaController {
+//    private LadaModell modell;
+//
+//    public LogikaiLadaController(LadaModell modell) {
+//        this.modell = modell;
+//    }
+//    
+    
+    
+    
     
 }
